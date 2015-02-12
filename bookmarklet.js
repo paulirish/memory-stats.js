6b3222574d92ec184d4ac121aed2f2a23e7ecc78
Olli Mahlamäki <olli.mahlamaki@reaktor.fi>
2015-02-12 08:18:13 +0200

    Updated links to master branch, do not leak globals

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,18 +1,20 @@
-var module;
-var script = document.createElement('script');
-script.onload = function() {
-  var stats = new MemoryStats();
+(function() {
+	var module;
+	var script = document.createElement('script');
+	script.onload = function() {
+	  var stats = new MemoryStats();
 
-    stats.domElement.style.position = 'fixed';
-    stats.domElement.style.right        = '0px';
-    stats.domElement.style.bottom       = '0px';
+	    stats.domElement.style.position = 'fixed';
+	    stats.domElement.style.right        = '0px';
+	    stats.domElement.style.bottom       = '0px';
 
-    document.body.appendChild( stats.domElement );
+	    document.body.appendChild( stats.domElement );
 
-    requestAnimationFrame(function rAFloop(){
-        stats.update();
-        requestAnimationFrame(rAFloop);
-    });
-};
-script.src = "https://cdn.rawgit.com/paulirish/memory-stats.js/b2fde44a6279d72dd272c04a01d1f67327906188/memory-stats.js";
-document.getElementsByTagName('head')[0].appendChild(script);
+	    requestAnimationFrame(function rAFloop(){
+	        stats.update();
+	        requestAnimationFrame(rAFloop);
+	    });
+	};
+	script.src = "https://rawgit.com/paulirish/memory-stats.js/master/memory-stats.js";
+	document.getElementsByTagName('head')[0].appendChild(script);
+}})();
