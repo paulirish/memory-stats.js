d92b617847104361166868c4bf7fa7345aa9fc06
Paul Irish <paul.irish@gmail.com>
2015-02-17 15:13:41 -0800

    bookmarklet: whitespace

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,20 +1,23 @@
 (function() {
-	var script = document.createElement('script');
-	script.onload = function() {
-	  var stats = new MemoryStats();
+  var script = document.createElement('script');
+  script.onload = function() {
 
-	    stats.domElement.style.position = 'fixed';
-	    stats.domElement.style.right        = '0px';
-	    stats.domElement.style.bottom       = '0px';
-	    stats.domElement.style.zIndex   = 100000;
+    var stats = new MemoryStats();
 
-	    document.body.appendChild( stats.domElement );
+    var elem = stats.domElement;
+    elem.style.position = 'fixed';
+    elem.style.right    = '0px';
+    elem.style.bottom   = '0px';
+    elem.style.zIndex   = 100000;
 
-	    requestAnimationFrame(function rAFloop(){
-	        stats.update();
-	        requestAnimationFrame(rAFloop);
-	    });
-	};
-	script.src = "https://rawgit.com/paulirish/memory-stats.js/master/memory-stats.js";
-	document.head.appendChild(script);
+    document.body.appendChild( stats.domElement );
+
+    requestAnimationFrame(function rAFloop(){
+      stats.update();
+      requestAnimationFrame(rAFloop);
+    });
+  };
+  
+  script.src = "https://rawgit.com/paulirish/memory-stats.js/master/memory-stats.js";
+  document.head.appendChild(script);
 })();
