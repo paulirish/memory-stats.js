17b389097e0e1a9c684bd02e30c217fb89a88d27
Paul Irish <paul.irish@gmail.com>
2015-02-24 11:18:25 -0800

    z-index

--- memory-stats.js
+++ memory-stats.js
@@ -12,7 +12,7 @@ var MemoryStats = function (){
 
 	var container	= document.createElement( 'div' );
 	container.id	= 'stats';
-	container.style.cssText = 'width:80px;height:48px;opacity:0.9;cursor:pointer;overflow:hidden;';
+	container.style.cssText = 'width:80px;height:48px;opacity:0.9;cursor:pointer;overflow:hidden;z-index:10000;';
 
 	var msDiv	= document.createElement( 'div' );
 	msDiv.id	= 'ms';
